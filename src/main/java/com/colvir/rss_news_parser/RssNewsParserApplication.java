eb72582c1df5cb4456898328caf787cd46511746
vvolski <valery.volski.colvir@gmail.com>
2024-08-19 22:37:44 +0300

    init project

---
+++ src/main/java/com/colvir/rss_news_parser/RssNewsParserApplication.java
@@ -0,0 +1,13 @@
+package com.colvir.rss_news_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssNewsParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssNewsParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colvir/rss_news_parser/RssNewsParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colvir.rss_news_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssNewsParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
