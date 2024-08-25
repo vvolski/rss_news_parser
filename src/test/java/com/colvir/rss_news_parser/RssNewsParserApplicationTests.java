e0375b39f8aa450aa84c9cfa92738f3eb7e9921b
vvolski <valery.volski.colvir@gmail.com>
2024-08-25 06:14:52 +0300

    Устранена проблема сборки приложения

--- src/test/java/com/colvir/rss_news_parser/RssNewsParserApplicationTests.java
+++ src/test/java/com/colvir/rss_news_parser/RssNewsParserApplicationTests.java
@@ -2,8 +2,10 @@ package com.colvir.rss_news_parser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "scheduler.enabled=false")
 class RssNewsParserApplicationTests {
 
 	@Test
